ee08eeff09d1da07332fc5cd095550ce5658115c
Stripey <tejas.ramdas@gmail.com>
2022-03-31 18:02:50 -0400

    added some buttons and stuff

--- patch_interface.jl
+++ patch_interface.jl
@@ -1,83 +1,67 @@
 using GLMakie, NIDAQ
 
-
-figure=Figure(backgroundcolor=RGBf(0.8,0.8,0.8),resolution=(800,600))
+figure=Figure(backgroundcolor=RGBf(0.8,0.8,0.8),resolution=(1200,900))
 
 a=figure[1,1:2] = GridLayout()
 b=figure[2,1:2] = GridLayout()
-c=figure[1:2,3] = GridLayout()
+c=figure[1:2,3] = GridLayout(tellwidth=false)
 
-ca=c[5,1] = GridLayout()
 
-ax_a=Axis(a[1,1],ylabel="Voltage")
-ax_b=Axis(b[1,1],xlabel="Time",ylabel="Current")
+ax_v=Axis(a[1,1],ylabel="Voltage",xlims=(-100,0), ylims=(-1,1))
+ax_i=Axis(b[1,1],xlabel="Time",ylabel="Current",xlims=(-100,0), ylims=(-1,1))
 
-mode = Toggle(ca[1,1], active = false) 
+mode = Observable(5.0)
+
+function voltage_to_mode(x)
+	axo_mode = [5, 2, 1, 4, 0, 3]
+	axo_desc = ["I-Clamp Fast", "I-Clamp", "I=0", "Track", "N/A", "V-Clamp"]
+	m=Int(round(x))
+	return axo_mode[m], axo_desc[m]
+end
+
+
+
+mode_label=Label(ca[1,1], "Mode: "*@lift(voltage_to_mode($mode)[2]), textsize=24, tellheight=false, tellwidth=false) 
+
+switch=Button(cd[1,1], label=@lift($recording ? "Stop recording" : "Start recording"), textsize=30, tellheight=false, tellwidth=false) 
+seal_test_button = Button(ca[1,1], label="Run seal test", textsize=30, tellheight=false,tellwidth=false)
+
+on(switch.clicks) do x
+	recording[] = 1-recording[]
+	println("Pressed")
+end
 
 
 on_width, offwidth, number, high_amplitude, low_amplitude
 frequency, cycle_time
 
 
-tail_plot=figure[1:5,3] = GridLayout()
-tail_viz=Axis(tail_plot[1,1])
+display_time_points=100
 
-tail_angles=Vector{GridLayout}(undef,5)
-tail_axes=Vector{Axis}(undef,5)
+v_vec=zeros(display_time_points)
+i_vec=zeros(display_time_points)
+t_vec=Vector(1:display_timepoints)*0.01
 
-for i in 1:5
-	tail_angles[i] = figure[i,1:2] = GridLayout()
-	tail_axes[i] = Axis(tail_angles[i][1,1])
-	if i!=5
-		hidexdecorations!(tail_axes[i])
-	end
-	hidexdecorations!(tail_axes[i], ticks=false,ticklabels=false)
-end
 
-data = Matrix(DataFrame(CSV.File("data.csv")))
+lines!(ax_v,@lift(t_vec[$time_index-display_time_points:$time_index]-t_vec[$time_index],v_vec[$time_index-display_time_points:$time_index]))
+lines!(ax_i,@lift(t_vec[$time_index-display_time_points:$time_index]-t_vec[$time_index],i_vec[$time_index-display_time_points:$time_index]))
 
-function get_bout(row_num,data)
-	t_max=findall(x->x===missing,data[row_num,:])[1]-1
-	bout_mat=convert(Matrix{Float32},reshape(data[row_num,1:size(data)[2]-3],(35,:))'[:,1:t_max])
-end
+recording=Observable(false)
 
-function plot_tail(ax,viz_ax,bout;dt=0.005)
-	seg_num=1
-	t=Observable(1)
-	plot_obs=on(t) do t
-		for seg_num = 1:5
-			empty!(ax[seg_num])
-			lines!(ax[seg_num],0:2:2*t-1,bout[seg_num,1:t[]],color=:black)
-			sleep(dt)
+#use button to toggle recording
+
+time_index=Observable(display_time_points)
+
+for i in 1:10000
+	datum = NIDAQ.read(daq_input)
+	if recording[]
+		#push!(v_vec,NIDAQ.read(daq_input)) #fix
+		#push!(t_vec,get_current_time()) #fix
+		if mode[] != Int(round(datum[end,3]))
+			println("Why would you change mode while recording!?!?!?")
 		end
-		seg_x=cumsum([0; sin.(bout[:, t[]])])
-		seg_y=-cumsum([0; cos.(bout[:, t[]])])
-		empty!(viz_ax)
-		lines!(viz_ax,seg_x,seg_y,color=:black)
 	end
-	xlims!(viz_ax,-3,3)
-	ylims!(viz_ax,-8,2)
-	for seg_num in 1:5
-		ylims!(ax[seg_num], minimum(bout[:,:]), maximum(bout[:,:]))
-		xlims!(ax[seg_num],0,size(bout)[2])
-	end
-	for i in 1:size(bout)[2]
-		t[]=i
-	end
+	mode[] = Int(round(datum[end, 3]))
+	sleep(0.2)
 end
-
-plot_tail(tail_axes,tail_viz,get_bout(1,data),dt=0.01)
-
-
-
-time_index=Observable(0)
-
-timestamps=Vector{Int}()
-data=Vector{Int}()
-
-lines(@lift(timestamps[time_index-20:time_index]-timestamps[time_index-20]),@lift(data[time_index-20:time_index]))
-
-
-for i=1:1000
-	push!(data,NIDAQ.read()) #fix
-	push!(timestamps,get_current_time()) #fix
+		
